fdff4f6430d15c83f74afca990ab9283c5e349f1
CodeCrave <omjadhav1303@gmail.com>
2026-01-23 16:45:55 +0530

    backend commit

---
+++ src/main/java/com/grievance_management/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.grievance_management.repository;
+
+import com.grievance_management.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    Optional<Employee> findByEmpnum(String empnum);
+}
